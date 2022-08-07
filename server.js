f576236b64f9c8727f15df35531b1c53be62f3dc
JMenon <adm7utd@gmail.com>
2022-08-07 12:30:18 +0530

    added comments

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ const express = require('express');
 
 const app = express();
 
+// Importing routers for route handling
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// Middlewares and Routes
+// App Level Middleware
+
+// for parsing requests
 app.use(express.urlencoded({ extended: true }));
+
+// for serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// imported router object with routes registered from routes/admin.js
-// order matters, if this was after '/' route middleware, we'd never
-// reach it.
+// Handling routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// For unhandled routes (404)
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
