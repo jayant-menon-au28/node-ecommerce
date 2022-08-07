bbacd4e81867e8de8d7893da7ad4ad5af0340432
JMenon <adm7utd@gmail.com>
2022-08-07 11:39:12 +0530

    created server with add product route

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Middlewares and Routes
+app.use(express.urlencoded({ extended: true }));
+
+// imported router object with routes registered from routes/admin.js
+// order matters, if this was after '/' route middleware, we'd never
+// reach it.
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// Starting the server
+app.listen(3000, () => {
+  console.log('Server listening...');
+});
