4957eff881019aef54f97dc0532157f3dcabf0ef
JMenon <adm7utd@gmail.com>
2022-08-08 12:05:44 +0530

    saving added products to file

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  products.push(req.body.title);
+  console.log(products);
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+  router: router,
+  prods: products,
+};
